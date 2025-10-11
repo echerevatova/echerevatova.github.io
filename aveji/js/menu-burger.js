a568a3b87eea405dab3af35920100487c3c526ee
echerevatova <135343074+echerevatova@users.noreply.github.com>
2025-10-11 11:54:25 +0300

    Add files via upload

--- aveji/js/menu-burger.js
+++ aveji/js/menu-burger.js
@@ -1,3 +1,9 @@
 document.querySelector('.header__hamburger').addEventListener('click', function() {
 	document.querySelector('.header').classList.toggle('animate');
 });
+
+document.querySelectorAll('.header__menu-link').forEach(link => {
+  link.addEventListener('click', function() {
+    document.querySelector('.header').classList.remove('animate');
+  });
+});
