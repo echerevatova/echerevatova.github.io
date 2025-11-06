7a8e884cbc0fea28583fff00394a7b562c57e222
echerevatova <135343074+echerevatova@users.noreply.github.com>
2025-11-06 13:57:29 +0300

    Delete Interpan directory

--- Interpan/js/burger-menu.js
+++
@@ -1,9 +0,0 @@
-document.querySelector('.header__hamburger').addEventListener('click', function() {
-	document.querySelector('.header').classList.toggle('animate');
-});
-
-document.querySelectorAll('.header__menu-links').forEach(link => {
-  link.addEventListener('click', function() {
-    document.querySelector('.header').classList.remove('animate');
-  });
-});
